4c1611f7b5d1e3aec9042a3f8ce66763ead5a35f
sura-z <169043346+sura-z@users.noreply.github.com>
2024-05-13 08:29:00 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,4 +1,6 @@
 immutable_var = 3, 5, 7, False, "String"
 print(immutable_var)
+#immutable_var [1] = 200
+#print(immutable_var) #объект кортежа не поддерживает назначение элементов
 mutable_list = [1, 2, 'a', 'b', 'Modified']
 print(mutable_list)
